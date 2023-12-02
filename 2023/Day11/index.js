5508730394c3044902e9be73592b54efd3cc53a4
David Brown <dtbrown@deloitte.co.uk>
2023-12-02 20:08:00 +0000

    skeleton of advents set up

---
+++ 2023/Day11/index.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+const input = fs.readFileSync("./", "utf8").trimEnd();
+// const input = fs.readFileSync("./", "utf8").trimEnd();
+
+function solutionOne(input) {
+	return 1;
+}
+
+function solutionTwo(input) {
+	return 2;
+}
+
+solutionOne(input);
+solutionTwo(input);
